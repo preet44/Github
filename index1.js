31516ce74617577b06ce5802d40890b684302a74
komal <preet@12gmail.com>
2026-04-16 17:51:20 +0530

    final commit

--- index1.js
+++ index1.js
@@ -1,20 +1,3 @@
+console.log("apn collge")
 
-
-
-let arr=["pineapple","apple","banana","orange"]
-let after_uppercase=arr.map((el)=>{
-   return el.toUpperCase();
-})
-console.log(after_uppercase);
-
-// function doubleAndReturn(arr){
-//     arr.map((el)=>{
-//         el*2;
-
-//     })
-
-   
-
-// }
-// console.log(doubleAndReturn([3,2,5,6]));
   
